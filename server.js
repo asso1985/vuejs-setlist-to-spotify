f07b7516c723b1c7833f9962087ebf7354eaed5e
Omar Assadi <assadi.omar.1985@gmail.com>
2017-09-11 10:54:50 +0200

    adde auth

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-// app.use(serveStatic(__dirname + '/dist/'));
-app.configure(function(){
-    app.use(express.bodyParser());
-    app.use(express.static(path.join(__dirname, 'dist')));
-});
+app.use(serveStatic(__dirname + 'dist'));
+console.log('> Starting production server...');
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
